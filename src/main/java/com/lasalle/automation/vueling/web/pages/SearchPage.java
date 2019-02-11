0c676e1219225fcb3b6439585185e0fbabfbe281
Alejandro Enríquez <alexalejandroem@gmail.com>
2019-02-11 19:18:36 +0100

    Added first field

--- src/main/java/com/lasalle/automation/vueling/web/pages/SearchPage.java
+++ src/main/java/com/lasalle/automation/vueling/web/pages/SearchPage.java
@@ -3,6 +3,7 @@ package com.lasalle.automation.vueling.web.pages;
 import com.lasalle.automation.vueling.web.domain.SearchDto;
 import net.serenitybdd.core.pages.PageObject;
 import net.serenitybdd.core.pages.WebElementFacade;
+import org.openqa.selenium.support.FindBy;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,7 +12,8 @@ import java.lang.invoke.MethodHandles;
 public class SearchPage  extends PageObject{
     private static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
-    private WebElementFacade txtOrigin;
+    @FindBy(css = ".searchbar-container .searchbar-main .searchbar-main_tab-content .searchbar-main_tab-content_pane .form-group>.form-input .input-group input")
+    private WebElementFacade Name;
     private WebElementFacade txtDestination;
     private WebElementFacade txtOutbound;
     private WebElementFacade txtReturn;
@@ -22,13 +24,13 @@ public class SearchPage  extends PageObject{
     public void searchFlight(SearchDto searchDto){
         LOGGER.debug("Search flight starts, search: [{}]", searchDto);
 
-        typeInto(txtOrigin, searchDto.getOrigin());
-        typeInto(txtDestination, searchDto.getDestination());
-        typeInto(txtOutbound, searchDto.getOutbound());
-        typeInto(txtReturn, searchDto.getReturnDate());
-        typeInto(txtPassengers, searchDto.getPassengers());
+        typeInto(Name, searchDto.getOrigin());
+        //typeInto(txtDestination, searchDto.getDestination());
+        //typeInto(txtOutbound, searchDto.getOutbound());
+        //typeInto(txtReturn, searchDto.getReturnDate());
+        //typeInto(txtPassengers, searchDto.getPassengers());
 
-        btnSearch.click();
+        //btnSearch.click();
 
     }
 
--- src/test/java/com/lasalle/automation/vueling/web/stepsdefs/search/SearchStepsDefs.java
+++ src/test/java/com/lasalle/automation/vueling/web/stepsdefs/search/SearchStepsDefs.java
@@ -1,5 +1,7 @@
 package com.lasalle.automation.vueling.web.stepsdefs.search;
 
+import com.lasalle.automation.vueling.web.domain.SearchDto;
+import com.lasalle.automation.vueling.web.pages.SearchPage;
 import cucumber.api.PendingException;
 import cucumber.api.java.After;
 import cucumber.api.java.Before;
@@ -14,6 +16,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.lang.invoke.MethodHandles;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class SearchStepsDefs {
@@ -22,13 +25,19 @@ public class SearchStepsDefs {
 
     private static WebDriver driver;
 
+    private String url ="https://www.vueling.com/es";
+
+
+    private SearchPage searchPage;
+
+    private List<SearchDto> searches;
+
     @Before
     public static void beforeClass() {
         System.setProperty("webdriver.chrome.driver", "/home/alejandro/Maestria/chromedriver_linux64/chromedriver");
         driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.manage().window().maximize();
-        driver.get("https://www.vueling.com/es");
         LOGGER.debug("driver started");
     }
 
@@ -41,10 +50,15 @@ public class SearchStepsDefs {
     @Given("^I'm main page$")
     public void iMMainPage(){
         LOGGER.debug("I'm in main page starts");
+        this.driver.get(this.url);
     }
 
     @When("^I try to find a fly$")
-    public void iTryToFindAFly() {
+    public void iTryToFindAFly(List<SearchDto> searchesDtoList) {
+        LOGGER.debug("iTryToFindAFly starts , list size [{}]", searchesDtoList.size());
+        searchPage.setDriver(this.driver);
+        searches = searchesDtoList;
+        searches.forEach( searchDto -> searchPage.searchFlight(searchDto));
     }
 
     @Then("^I get available flight$")
